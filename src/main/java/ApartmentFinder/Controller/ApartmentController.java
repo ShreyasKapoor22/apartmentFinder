a8533918751ca2c86c4af318ca8776cfb6697982
shreyaskapoor22 <shreyas.kapoor@colorado.edu>
2022-04-11 20:07:48 -0600

    updated code

--- src/main/java/ApartmentFinder/Controller/ApartmentController.java
+++ src/main/java/ApartmentFinder/Controller/ApartmentController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -22,9 +24,15 @@ public class ApartmentController {
 
 
     @GetMapping("/list")
-    public String showApartmentList(Model theModel) {
+    public String showApartmentList( Model theModel, HttpServletRequest request) {
+        System.out.println("Inside showApartment List");
         List<Apartment> apartmentList = apartmentService.getApartmentList();
+        User user = (User) request.getSession().getAttribute("userData");
+        assert user != null;
+        System.out.println("User data fetched in apartment list = "+ user.getUserName());
+
         theModel.addAttribute("apartmentList", apartmentList);
+        theModel.addAttribute("userData", user);
         System.out.println("No of apartments = " + apartmentList.size());
         return "apartments";
     }
